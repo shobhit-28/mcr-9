31d00130a21491a6ea088136f485bf1146cf95af
shobhit-28 <shobhitraj34@gmail.com>
2023-07-29 12:24:57 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="App">
+      <p className="text-3xl">Hello</p>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        min_xl: { 'min': '1215px' },
+        xl: { 'max': '1215px' },
+        lg: { 'max': '1024px' },
+        md: { 'max': '900px' },
+        sm: { 'max': '450px' },
+        xs: { 'max': '340px' },
+      },
+      extend: {},
+    },
+  },
+  plugins: [],
+}
+
