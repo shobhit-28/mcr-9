37ecb1512d1e054bafba1cd193be47ef86982f4a
shobhit-28 <shobhitraj34@gmail.com>
2023-07-29 13:47:15 +0530

    sidebar completed

--- src/components/sideBar/sideBar.jsx
+++ src/components/sideBar/sideBar.jsx
@@ -1,29 +1,28 @@
-import { useNavigate } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 import {AiFillCompass, AiOutlineClockCircle, AiTwotoneHome} from 'react-icons/ai'
 import {RiPlayListAddFill} from 'react-icons/ri'
 
 export const SideBar = () => {
-    const navigate = useNavigate()
 
   return (
     <div className='fixed w-48 h-full pl-4 flex flex-col gap-6 pt-36 shadow-[0_1px_5px_rgb(0,0,0,0.2)]'>
-        <p className="flex gap-2 items-center text-xl cursor-pointer" onClick={() => navigate('/')}>
+        <NavLink className={({isActive}) => `flex gap-2 items-center text-xl ${isActive && 'text-sky-600 italic'}`} to={'/'}>
             <AiTwotoneHome />
             <span className="">Home</span>
-        </p>
-        <p className="flex gap-2 items-center text-xl cursor-pointer" onClick={() => navigate('/explore')}>
+        </NavLink>
+        <NavLink className={({isActive}) => `flex gap-2 items-center text-xl ${isActive && 'text-sky-600 italic'}`} to={'/explore'}>
             <AiFillCompass />
             <span className="">Explore</span>
-        </p>
-        <p className="flex gap-2 items-center text-xl cursor-pointer" onClick={() => navigate('/watchlater')}>
+        </NavLink>
+        <NavLink className={({isActive}) => `flex gap-2 items-center text-xl ${isActive && 'text-sky-600 italic'}`} to={'/playlist'}>
             <RiPlayListAddFill />
             <span className="">Playlists</span>
-        </p>
-        <p className="flex gap-2 items-center text-xl cursor-pointer" onClick={() => navigate('/playlist')}>
+        </NavLink>
+        <NavLink className={({isActive}) => `flex gap-2 items-center text-xl ${isActive && 'text-sky-600 italic'}`} to={'/watchlater'}>
             <AiOutlineClockCircle />
             <span className="">Watch later</span>
-        </p>
+        </NavLink>
     </div>
   )
 }
